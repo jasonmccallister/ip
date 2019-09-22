39a984059581e9eba37d28f61ad2fc75af87151f
Jason McCallister <themccallister@gmail.com>
2019-09-22 16:49:17 -0400

    wip

---
+++ main.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+)
+
+type response struct {
+	IP string `json:"ip,omitempty"`
+}
+
+func main() {
+	log.Print("IP microservice started")
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		ip := header(r)
+		resp := response{IP: ip}
+
+		js, err := json.Marshal(resp)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+
+		w.Header().Set("Content-Type", "application/json")
+		w.Write(js)
+	})
+
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+	}
+
+	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%s", port), nil))
+}
+
+func header(r *http.Request) string {
+	switch r.Header.Get("X-Forwarded-For") {
+	case "":
+		return r.RemoteAddr
+	}
+
+	return r.Header.Get("X-Forwarded-For")
+}
