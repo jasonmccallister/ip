eaf853d4ae340ef972624579d44f6c66290438cf
Jason McCallister <themccallister@gmail.com>
2019-09-22 17:00:14 -0400

    remove the nested handler

--- api/ip.go
+++ api/ip.go
@@ -2,10 +2,8 @@ package ip
 
 import (
 	"encoding/json"
-	"fmt"
 	"log"
 	"net/http"
-	"os"
 )
 
 type response struct {
@@ -15,27 +13,17 @@ type response struct {
 // Handler is the endpoint that creates the serverless function for Zeit
 func Handler(w http.ResponseWriter, r *http.Request) {
 	log.Print("IP microservice started")
+	ip := header(r)
+	resp := response{IP: ip}
 
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		ip := header(r)
-		resp := response{IP: ip}
-
-		js, err := json.Marshal(resp)
-		if err != nil {
-			http.Error(w, err.Error(), http.StatusInternalServerError)
-			return
-		}
-
-		w.Header().Set("Content-Type", "application/json")
-		w.Write(js)
-	})
-
-	port := os.Getenv("PORT")
-	if port == "" {
-		port = "8080"
+	js, err := json.Marshal(resp)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
 	}
 
-	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%s", port), nil))
+	w.Header().Set("Content-Type", "application/json")
+	w.Write(js)
 }
 
 func header(r *http.Request) string {
